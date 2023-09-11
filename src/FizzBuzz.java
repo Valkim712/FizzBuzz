8254910fdc3ec2c7cbb5a72fe38a9786f3d0050a
Valerie Kim <val.kim712@gmail.com>
2023-09-11 10:43:30 -0400

    modified

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -8,32 +8,29 @@ class FizzBuzz {
         while (i < 100) {
 
             // Find out which numbers divide i.
-            doFizzBuzz(i);
-            i++;
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
+
+                System.out.println("Fizz Buzz");
+
+            } else if (divisibleBy3) {
+
+                System.out.println("Fizz");
+
+            } else if (divisibleBy5) {
+
+                System.out.println("Buzz");
+
+            } else {
+
+                System.out.println(i);
+
+            }
+        i++;
         } 
     }
-
-    private static void doFizzBuzz(int i) {
-        boolean divisibleBy3 = i % 3 == 0;
-        boolean divisibleBy5 = i % 5 == 0;
-
-        // Print our appropriate result.
-        if (divisibleBy3 && divisibleBy5) {
-
-            System.out.println("Fizz Buzz");
-
-        } else if (divisibleBy3) {
-
-            System.out.println("Fizz");
-
-        } else if (divisibleBy5) {
-
-            System.out.println("Buzz");
-
-        } else {
-
-            System.out.println(i);
-
-        }
-    }
 }
+yay
